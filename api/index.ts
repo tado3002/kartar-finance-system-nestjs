f60c1cd35aae5f3bbdc4c4cf1f3c2beb7526e56d
tdh.schwarzen <muh.murtadlo23@gmail.com>
2025-07-13 21:49:35 +0700

    Update api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
 
-let cachedServer: Server;
+// Simpan instance Express (tidak null)
+let cachedApp: any = null;
 
-async function bootstrapServer(): Promise<Server> {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({
-    origin: ['http://localhost:8080'],
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-  await app.init();
-  return createServer(app.getHttpAdapter().getInstance());
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
+    app.enableCors({
+      origin: ['http://localhost:8080'],
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
+    await app.init();
+    cachedApp = app.getHttpAdapter().getInstance();
+  }
+  return cachedApp;
 }
 
-export default async function handler(req, res) {
-  if (!cachedServer) {
-    cachedServer = await bootstrapServer();
-  }
-  return proxy(cachedServer, req, res);
+export default async function handler(req: any, res: any) {
+  const app = await bootstrap();
+  // app tidak akan null di sini
+  return app(req, res);
 }
